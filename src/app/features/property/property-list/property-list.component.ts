0380b64edc241814c94968162a5b1b3560300519
ayoubbelhaj <ayoubbelhaj2017@gmail.com>
2024-10-25 22:45:47 +0100

    feat(property): add property list design

--- src/app/features/property/property-list/property-list.component.ts
+++ src/app/features/property/property-list/property-list.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor,CommonModule],
   templateUrl: './property-list.component.html',
   styleUrl: './property-list.component.css'
 })
 export class PropertyListComponent {
-
+  properties = [
+    { title: 'Large 4-room apartment with a beautiful terrace', price: '2500 DTN', location: 'Nabeul', img: '../../../../assets/images/maison.png' },
+    { title: '5i large design apartment with terrace', price: '3000 DTN', location: 'Tunis', img: '../../../../assets/images/maison3.png' },
+    { title: 'Magnificent duplex in a private villa', price: '1500 DTN', location: 'Sousse', img: '../../../../assets/images/maison2.png'},
+    { title: 'Large 4-room apartment with a beautiful terrace', price: '2500 DTN', location: 'Nabeul', img: '../../../../assets/images/maison.png' },
+    { title: 'Card title 5', price: '3000 DTN', location: 'Tunis', img: '../../../../assets/images/maison3.png' },
+    { title: 'Card title 6', price: '1500 DTN', location: 'Sousse', img: '../../../../assets/images/maison2.png' },
+    { title: 'Large 4-room apartment with a beautiful terrace', price: '2500 DTN', location: 'Nabeul', img: '../../../../assets/images/maison.png' },
+    { title: '5i large design apartment with terrace', price: '3000 DTN', location: 'Tunis', img: '../../../../assets/images/maison3.png' },
+    { title: 'Magnificent duplex in a private villa', price: '1500 DTN', location: 'Sousse', img: '../../../../assets/images/maison2.png' },
+  ];
 }
